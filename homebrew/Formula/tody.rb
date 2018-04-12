7363faaba402c89bcd46f88abb2c2fe93c130cd3
arnolda <>
2018-04-12 14:06:04 +1000

    first brew release

--- homebrew/Formula/tody.rb
+++ homebrew/Formula/tody.rb
@@ -2,7 +2,7 @@ class Dk < Formula
     desc ""
     homepage "https://github.com/todemy/tody-cli"
     url "https://github.com/todemy/tody-cli/blob/master/homebrew/Release/tody-0.0.1.tar.gz?raw=true"
-    sha256 ""
+    sha256 "8ac6959f105256f45006f84014755498ed9703a641000e4b1c7de07be4b0bdac"
   
     def install
       bin.install "tody"
